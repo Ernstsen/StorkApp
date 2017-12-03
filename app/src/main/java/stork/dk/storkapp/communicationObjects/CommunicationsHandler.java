ea5ef395743073e1687cd105375ad684d7824937
Johannes Ernstsen <johannes.ernstsen@post.au.dk>
2017-12-03 20:07:58 +0100

    added gson and made CommunicationsHandler for server communication

---
+++ app/src/main/java/stork/dk/storkapp/communicationObjects/CommunicationsHandler.java
@@ -0,0 +1,10 @@
+package stork.dk.storkapp.communicationObjects;
+
+/**
+ * @author Johannes
+ */
+
+public class CommunicationConstants {
+    public static final String BASE_URL = "http://e-software.dk:8080/";
+    public static final String LOGIN_URL = "login";
+}
---
+++ app/src/main/java/stork/dk/storkapp/communicationObjects/Constants.java
@@ -0,0 +1,8 @@
+package stork.dk.storkapp.communicationObjects;
+
+/**
+ * Created by Johannes on 03-12-2017.
+ */
+
+public class Constants {
+}
