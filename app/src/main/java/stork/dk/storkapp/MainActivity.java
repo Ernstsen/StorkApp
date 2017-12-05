6885bb250d9454d3a6fbe1a0ec57f75327764b09
mathiasjensen <darkdawnmsj@gmail.com>
2017-12-05 10:14:38 +0100

    Changed backPress on MainActivity, changed styling on Settings activity

--- app/src/main/java/stork/dk/storkapp/MainActivity.java
+++ app/src/main/java/stork/dk/storkapp/MainActivity.java
@@ -191,11 +191,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-        finish();
         super.onBackPressed();
+        finish();
     }
 
     public void checkIfLoggedIn(){
