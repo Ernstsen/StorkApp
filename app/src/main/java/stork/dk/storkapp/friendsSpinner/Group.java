ee2c7baa497d53f8ee3b489606ac5545b43b20c7
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2017-12-07 13:58:15 +0100

    updated Group to be the same as server

--- app/src/main/java/stork/dk/storkapp/friendsSpinner/Group.java
+++ app/src/main/java/stork/dk/storkapp/friendsSpinner/Group.java
@@ -6,17 +6,19 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * @author morten
+ * @author morten, johannes
  */
 public class Group implements Traceable, Serializable, Comparable {
     private int id;
     private String name;
     private List<Friend> friends;
+    private int owner;
 
-    public Group(int id, String name, List<Friend> friends) {
+    public Group(int id, String name, List<Friend> friends, int owner) {
         this.id = id;
         this.name = name;
         this.friends = friends;
+        this.owner = owner;
     }
 
     public int getId() {
@@ -29,6 +31,26 @@ public class Group implements Traceable, Serializable, Comparable {
 
     public List<Friend> getFriends() {
         return friends;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFriends(List<Friend> friends) {
+        this.friends = friends;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
     }
 
     //to display object as a string in spinner
